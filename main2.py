85a09036df789e759f5386bf06c78c1696de5cbd
Ali <hisseiny@outlook.fr>
2025-10-02 14:08:41 +0000

    module2 python

---
+++ main2.py
@@ -0,0 +1,30 @@
+# # TODO: Create a list of your favorite fruits
+# fruits = ["Orange", "Mango", "grapes"]
+
+# # TODO: Add a new fruit to the end of the list
+# fruits.append("Banana")
+
+# # TODO: Remove the second fruit from the list
+# fruits.pop(1)
+
+# # TODO: Sort the list alphabetically
+# fruits.sort()
+
+# # TODO: Create a new list with the first three fruits
+# newFruits = ["Orange", "Mango", "grapes"]
+
+# # Print the original list and the new list
+# print(newFruits)
+
+# TODO: Create a tuple with your favorite colors
+fruits = ("Orange", "Yellow", "Blue")
+
+# TODO: Try to modify one of the colors (this should raise an error)
+
+# TODO: Count how many times a specific color appears in the tuple
+
+# TODO: Find the index of a specific color
+
+# TODO: Create a new tuple by concatenating two existing tuples
+
+# Print the results of each operation
